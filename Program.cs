000bb835acd6eee6f0214572387301d4e72eecdf
Savioli Pri <stobbeglaucya@gmail.com>
2021-09-16 15:49:16 -0300

    Adicionar solução exercício Cebolinha

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String frasedigitada, frasecomdislalia;
+            Console.Write("Digite um Flase:");
+            frasedigitada = Console.ReadLine();
+
+            frasecomdislalia = frasedigitada.Replace("r", "l").Replace("R", "L");
+
+            Console.WriteLine(frasecomdislalia);
+
+
+            
+        }
+    }
+}
